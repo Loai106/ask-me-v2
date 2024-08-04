e1f6c014e43831c9359a947b94a4f74e6febaac0
Loai <43700105+Loai106@users.noreply.github.com>
2024-08-04 20:25:00 +0300

    ordering the inbox page's posst

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -15,6 +15,10 @@ export default async function page() {
     where: {
       userId: session?.user.id,
     },
+    orderBy:{
+      createdAt:"desc"
+    }
+    
   });
 
   const renderedQuestions = receivedQuestions.map(async (question) => {
